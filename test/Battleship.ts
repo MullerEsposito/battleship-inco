13fea5194d6cd0de2fec78cb2594868518416eb3
Muller Esposito <mulleresposito@hotmail.com>
2024-07-13 10:39:38 -0300

    it adds the pre battles tests;

--- test/Battleship.ts
+++ test/Battleship.ts
@@ -1,9 +1,10 @@
 import hre from "hardhat";
 import { expect } from "chai";
+import { AddressLike, ContractFactory } from "ethers";
 
-let battleship;
-let player1;
-let player2;
+let battleship: any;
+let player1: AddressLike;
+let player2: AddressLike;
 
 beforeEach(async function () {
   // Contracts are deployed using the first signer/account by default
@@ -13,4 +14,27 @@ beforeEach(async function () {
 
   const Battleship = await hre.ethers.getContractFactory("Battleship");
   battleship = await Battleship.deploy(player1, player2);
+});
+
+describe("Battleship", function () {
+  describe("Pre battle tests", function () {
+    it("Should be create the players in the game", async () => {
+      expect(await battleship.player1()).to.equal(player1);
+      expect(await battleship.player2()).to.equal(player2);
+    });
+
+    it("verifies if player 1 is the first to play", async () => {
+      expect(await battleship.currentPlayer()).to.equal(player1);
+    });
+
+    it("certifies that the game and players aren't ready yet", async () => {
+      expect(await battleship.player1Ready()).to.equal(false);
+      expect(await battleship.player2Ready()).to.equal(false);
+      expect(await battleship.gameReady()).to.equal(false);
+    });
+  });
+
+  describe("Setting battle tests", function () {
+    it("", () => {});
+  });
 });
