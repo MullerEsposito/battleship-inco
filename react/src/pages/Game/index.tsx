5d2f6019201ed47b04e4d2f70d16f222ed27cd01
mulleresposito <mulleresposito@hotmail.com>
2024-07-13 23:34:35 -0300

    refact(Game): it refact Wallet Connect Info;

--- react/src/pages/Game/index.tsx
+++ react/src/pages/Game/index.tsx
@@ -3,12 +3,14 @@ import { Container, Explosion, FireButton, GameContainer, Ocean, Ship, Torpedo, 
 
 import shipImg from "../../assets/ship.png";
 import { useWalletInfo, useWeb3ModalAccount } from "@web3modal/ethers5/react";
+import { usePrivy } from "@privy-io/react-auth";
 
 export function Game() {
   const [fire, setFire] = useState(false);
   const [explode, setExplode] = useState(false);
   const { walletInfo } = useWalletInfo();
   const { address, isConnected } = useWeb3ModalAccount()
+  const { user, authenticated} = usePrivy();
   
   const fireTorpedo = () => {
     setFire(false);
@@ -23,7 +25,12 @@ export function Game() {
       {isConnected && (
         <WalletConnectInfo>
           <img src={walletInfo?.icon} alt="" />
-          You're connected with <span>{walletInfo?.name}</span> in the Wallet: <span>{address?.slice(0,5) + "..." + address?.slice(-5)}</span>
+          You're connected through Wallet Connect SDK with <span>{walletInfo?.name}</span> in the Wallet: <span>{address?.slice(0,5) + "..." + address?.slice(-5)}</span>
+        </WalletConnectInfo>
+      )}
+      {authenticated && user?.wallet && (
+        <WalletConnectInfo>
+          You're connected through Privy SDK with <span>{user.wallet.walletClientType}</span> in the Wallet: <span>{user.wallet.address?.slice(0,5) + "..." + user.wallet.address?.slice(-5)}</span>
         </WalletConnectInfo>
       )}
 
